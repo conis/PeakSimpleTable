7a83e582810a8ebb49133542a8ae0c72e950b191
Conis <conis.yi@gmail.com>
2013-08-23 20:47:40 +0800

    项目初始化

---
+++ PeakSimpleTable-Samples/PeakSimpleTable-Samples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PeakSimpleTable-Samples
+//
+//  Created by conis on 8/23/13.
+//  Copyright (c) 2013 conis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PeakSimpleTable-Samples/PeakSimpleTable-Samples/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PeakSimpleTable-Samples
+//
+//  Created by conis on 8/23/13.
+//  Copyright (c) 2013 conis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
